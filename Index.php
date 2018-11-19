a4897f7f7019a87a31fd28691201f0b698053118
tzimpoulog <tzimpoulasnikos@gmail.com>
2018-11-19 11:24:44 +0200

    Add header.php footer.php page.php single.php files

--- Index.php
+++ Index.php
@@ -1,5 +1,12 @@
+<?php get_header();
 
-/*
-Author: tzibo
-Version: 1.o
- */
+
+  while(have_posts()){
+    the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php the_content(); ?>
+    <hr>
+  <?php }
+
+get_footer();
+?>
